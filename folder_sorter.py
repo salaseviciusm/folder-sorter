904bdf5d486e8cd12ec6cf7de3b3b36e46283756
Morkus <ms1419@imperial.ac.uk>
2020-02-12 16:21:26 +0000

    implement folder sorting for non-tagged files and refactored code

--- folder_sorter.py
+++ folder_sorter.py
@@ -26,61 +26,69 @@ class EventHandler(FileSystemEventHandler):
             # Checks if @filename contains a prefixed tag
             if not tag == None:
                 tag = tag.group()
-                if tag in self.dest_tags:
-                    new_name = filename.split(tag)[1].strip()
-                    new_destination = self.dest_tags[tag] + "/" + new_name
-
-                    #src = self.source_dir + "/" + filename
-                    #self.move_file(src, new_destination)
-
-                    file_exits = os.path.isfile(new_destination)
-                    i = 0
-                    # Renames the file with an incremental value if @filename already exists
-                    while (file_exits):
-                        i += 1
-                        new_name = filename.split(tag)[1].strip() + " " + str(i)
-                        new_destination = self.dest_tags[tag] + "/" + new_name
-                        file_exits = os.path.isfile(new_destination)
+                if tag.upper() in self.dest_tags:
+                    new_destination = self.dest_tags[tag.upper()]
                 else:
                     new_dir = self.destination_dir + "/" + tag
-                    os.mkdir(new_dir)
-                    self.dest_tags[tag] = new_dir
 
-                    new_destination = new_dir + "/" + filename.split(tag)[1].strip()
+                    self.dest_tags[tag.upper()] = new_dir
+                    os.mkdir(new_dir)
+
+                    new_destination = new_dir
+
+                # removes the tag from the filename
+                new_destination += "/" + filename.split(tag)[1].strip()
             else:
-                if filename.endswith(".exe") or filename.endswith(".msi"):
-                    pass
-                continue
-                # TODO: check for file type and place into appropriate folder in downloads
+                if not os.path.isfile(self.source_dir + "/" + filename):
+                    continue
+                extension = re.search("(.\w+)$", filename)
+                assert not extension == None
+                extension = extension.group()
+
+                new_destination = self.source_dir
+                if extension in [".exe", ".msi"]:
+                    new_destination += "/executables"
+                elif extension in [".pdf"]:
+                    new_destination += "/pdfs"
+                elif extension in [".png", ".jpg", ".jpeg"]:
+                    new_destination += "/images"
+                elif extension in [".zip", ".jar", ".tgz"]:
+                    new_destination += "/zips"
+                else:
+                    new_destination += "/other"
+
+                if not os.path.exists(new_destination):
+                    os.mkdir(new_destination)
+
+                new_destination += "/" + filename
 
             src = self.source_dir + "/" + filename
-            os.rename(src, new_destination)
+            self.move_file(src, new_destination)
             print("File: '"+filename+"' moved to: '" + new_destination + "'")
 
-    #def move_file(self, src, dest):
-    #    new_destination = dest
+    def move_file(self, src, dest):
+        new_destination = dest
 
         # Renames the file with an incremental value if @dest already exists
-    #    file_exists = os.path.isfile(new_destination)
-    #    i = 0
-    #    while (file_exits):
-    #        i += 1
-    #        new_destination= dest + " " + str(i)
-    #        file_exits = os.path.isfile(new_destination)
+        file_exists = os.path.isfile(new_destination)
+        i = 0
+        while (file_exists):
+            i += 1
+            new_destination= dest + " " + str(i)
+            file_exits = os.path.isfile(new_destination)
 
-    #    os.rename(src, new_destination)
-    #    filename = src.split("/")[-1]
-    #    print("File: '"+filename+"' moved to: '" + new_destination + "'")
+        os.rename(src, new_destination)
+        filename = src.split("/")[-1]
+        print("File: '"+filename+"' moved to: '" + new_destination + "'")
 
-
-
+    # Builds a dictionary of tags to their directory path
     def get_dest_dir_heirarchy(self):
         for dir in os.listdir(self.destination_dir):
             dir_path = self.destination_dir + "/" + dir
             if os.path.isdir(dir_path):
                 tag = re.search(EventHandler.SEARCH_PATTERN, dir)
                 if not tag == None:
-                    self.dest_tags[tag.group()] = dir_path
+                    self.dest_tags[tag.group().upper()] = dir_path
 
 
 if __name__ == "__main__":
